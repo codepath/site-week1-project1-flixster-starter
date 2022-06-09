7f38fef622d5281cfe9e4f575e6951169b7b41ea
Annesa Tran <annesat2020@gmail.com>
2022-06-09 14:46:31 -0700

    Added ability to clear search results

--- README.md
+++ README.md
@@ -23,14 +23,14 @@ Deployed Application (optional): [Flixster Deployed Site](ADD_LINK_HERE)
   - The button should have an id of `load-more-movies-btn`.
   - When clicked, the page should not refresh.
   - New movies should simply be added to the bottom
-- [ ] Allow users to search for movies and display them in a grid view
+- [x] Allow users to search for movies and display them in a grid view
   - There should be a search input element with an id of `search-input`
   - Users should be able to type into the input
   - When a user hits 'Enter', it should send a search request to the movies API
   - The results from the search should be displayed on the page
   - There should be a close icon with an id of `close-search-btn` that exits the search, clears results, and shows the current movies displayed previously
-- [ ] Website accounts for basic HTML/CSS accessibility features
-- [ ] Website should be responsive
+- [x] Website accounts for basic HTML/CSS accessibility features
+- [x] Website should be responsive
 
 #### STRETCH FEATURES
 
--- script.js
+++ script.js
@@ -1,75 +1,96 @@
-// global constants
-API_KEY = "7588252b35a0bbe04a7685828388c9e8"
-BASE_API_URL  = "https://api.themoviedb.org/3"
-API_KEY_QUERY = "?api_key=" + API_KEY
-
-// variables
-let searchTerm = "";
-let currentPage = 1;
-
-// DOM selectors
-const closeSearchBtn = document.querySelector("#close-serach-btn");
-const moviesGrid = document.querySelector("#movies-grid");
-const loadMoreBtn = document.querySelector("#load-more-movies-btn");
-const searchBtn = document.querySelector("#clicker");
-
-// event listeners
-//closeSearchBtn.addEventListener("click", closeSearch);
-loadMoreBtn.addEventListener("click", loadMoreMovies);
-searchBtn.addEventListener("click", handleSearchFormSubmit);
-
-async function getNowPlaying (){
-    let apiURL = BASE_API_URL + "/movie/now_playing" + API_KEY_QUERY + "&page=" + currentPage;
-    console.log("called URL:", apiURL);
-    
-    response = await fetch(apiURL);
-    console.log("response:", response)
-
-    let resultsData = await response.json();
-    console.log("responseData:", resultsData);
-
-    displayResults(resultsData)
-}
-
-async function getResults (apiRequestURL){
-    response = await fetch(apiRequestURL);
-    let resultsData = await response.json();
-    displayResults(resultsData)
-}
-
-//will need to edit this if i want to show more details when they clickor hover on it
-
-function displayResults (resultsData){
-    movieResults = resultsData.results 
-    movieResults.forEach( movie => 
-        moviesGrid.innerHTML += `
-        <div class="movie-card">
-            <img class="movie-poster" src="${"https://image.tmdb.org/t/p/w500"+movie.poster_path}">
-            <div class="movie-card-info">
-                <h2 class="movie-title">${movie.title}</h2>
-                <h3 class="movie-votes">${movie.vote_average}</h3>
-            </div>
-        </div>
-    `
-)}
-
-function handleSearchFormSubmit (evt){
-    evt.preventDefault();
-    moviesGrid.innerHTML = ''
-    currentPage = 1;
-    
-    searchTerm = document.getElementById('search-input').value;
-    getResults(BASE_API_URL + "/search/movie" + API_KEY_QUERY + "&query=" + searchTerm)
-}
-
-// need to edit this so that it loads more movies in general, not just the Now Playing
-async function loadMoreMovies (evt){
-    currentPage += 1;
-    getNowPlaying();
-}
-
-
-window.onload = function () {
-    currentPage = 1;
-    getNowPlaying();
-  }
+ // global constants
+ API_KEY = "7588252b35a0bbe04a7685828388c9e8"
+ BASE_API_URL  = "https://api.themoviedb.org/3"
+ API_KEY_QUERY = "?api_key=" + API_KEY
+ 
+ // variables
+ let searchTerm = "";
+ let currentPage = 1;
+ 
+ // DOM selectors
+ const closeSearchBtn = document.querySelector("#close-search-btn");
+ const moviesGrid = document.querySelector("#movies-grid");
+ const loadMoreBtn = document.querySelector("#load-more-movies-btn");
+ const searchBtn = document.querySelector("#clicker");
+ 
+ 
+ // TESTER
+ let latestApiCall = ""
+ currentPage = 1
+ 
+ // event listeners
+ closeSearchBtn.addEventListener("click", closeSearch);
+ loadMoreBtn.addEventListener("click", loadMoreMovies);
+ searchBtn.addEventListener("click", handleSearchFormSubmit);
+ 
+ async function getResults (apiRequestURL){
+     latestApiCall = apiRequestURL
+     console.log("called URL:", apiRequestURL);
+     let response = await fetch(apiRequestURL);
+     console.log("response:", response)
+     let resultsData = await response.json();
+     console.log("resultsData:", resultsData);
+     
+     return resultsData
+ }
+ 
+ function displayResults (resultsData){
+     movieResults = resultsData.results 
+     movieResults.forEach( movie => 
+         moviesGrid.innerHTML += `
+         <div class="movie-card">
+             <img class="movie-poster" src="${"https://image.tmdb.org/t/p/original"+movie.poster_path}">
+             <div class="movie-card-info">
+                 <h2 class="movie-title">${movie.title}</h2>
+                 <h3 class="movie-votes">${movie.vote_average}</h3>
+             </div>
+         </div>
+     `
+ )}
+ 
+ async function displayNowPlaying (){
+     results = await getResults(BASE_API_URL + "/movie/now_playing" + API_KEY_QUERY);
+     displayResults(results);
+ }
+ 
+ async function handleSearchFormSubmit (evt){
+     // prevent from reloading
+     evt.preventDefault();
+     
+     // clear current movies so results show up at the top
+     moviesGrid.innerHTML = "";
+ 
+     // reset current page to 1
+     currentPage = 1;
+ 
+     // grab input from search form
+     searchTerm = document.getElementById('search-input').value;
+     
+     let apiRequestURL = BASE_API_URL + "/search/movie" + API_KEY_QUERY + "&query=" + searchTerm
+     results = await getResults(apiRequestURL)
+ 
+     displayResults(results)
+ 
+     // show close search button
+     closeSearchBtn.classList.remove("hidden");
+ 
+ }
+ 
+ async function loadMoreMovies (){
+     currentPage += 1
+     results = await getResults(latestApiCall+"&page=" + currentPage)
+     console.log(results)
+     displayResults(results)
+ }
+ 
+ function closeSearch (evt){
+     moviesGrid.innerHTML = "";
+     currentPage = 1;
+     displayNowPlaying();
+     closeSearchBtn.classList.add("hidden");
+ }
+ 
+ window.onload = function () {
+     currentPage = 1;
+     displayNowPlaying();
+   }
