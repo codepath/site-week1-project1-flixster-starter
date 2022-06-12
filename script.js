10ca29777b7314d8a63bc821e9aac552c5bfa1a4
EvansAsuboahStetson <EvansAsuboahStetson@users.noreply.github.com>
2022-06-12 04:20:34 -0400

    Updated the popup

--- script.js
+++ script.js
@@ -23,40 +23,10 @@ async function getMovie_data(page_Num) {
 async function getAllMovies() {
   const responseData = await getMovie_data(page_Num)
   console.log(responseData)
-  display(responseData)
+  await display(responseData)
 }
-function display(responseData) {
-  if (responseData) {
-    responseData.results.forEach((element) => {
-      var image_path = ''
-
-      if ('poster_path' in element) {
-        image_path = element.poster_path
-      }
-      var id = element.id
-
-      const title = element.title
-      const votes = element.vote_average
-
-      grid_cont.innerHTML += `
-      
-      <div class= "movie-card" id="${id}">
-      <img class= "movie-poster" src= "https://image.tmdb.org/t/p/w500/${image_path}"/>
-      <div class = "rating">
-      <div class= "star">⭐</div>
-      <div class= "movie-votes"> ${votes}</div>
-      </div>
-      <div class="movie-title">${title}</div>
-      </div>
-      `
-    })
-  }
-}
-
 
 //#\33
-
-
 
 async function getSearchMovies(evt) {
   const user_input = search_bar.value
@@ -115,11 +85,13 @@ async function LoadMore(evt) {
   console.log(load_data)
   display(load_data)
 }
-const search_icon = document.querySelector('.search_icon').addEventListener('click', displaySearch)
+const search_icon = document
+  .querySelector('.search_icon')
+  .addEventListener('click', displaySearch)
 
 const back_button = document.querySelector('.close-search-btn')
 
-back_button.addEventListener('click',backFunc)
+back_button.addEventListener('click', backFunc)
 
 function displaySearch(evt) {
   evt.preventDefault()
@@ -127,35 +99,106 @@ function displaySearch(evt) {
   let visibility = window.getComputedStyle(search_bar).visibility
 
   if (visibility == 'hidden') {
-      search_bar.setAttribute('style', 'visibility : visible')
-      back_button.setAttribute('style','visibility:visible')
-      
+    search_bar.setAttribute('style', 'visibility : visible')
+    back_button.setAttribute('style', 'visibility:visible')
   }
 }
 
-function backFunc(evt)
-{
-    evt.preventDefault()
-    search_bar.value = ""
-     search_bar.setAttribute('style', 'visibility : hidden')
-    back_button.setAttribute('style', 'visibility:hidden')
-    myFunction()
-    
+function backFunc(evt) {
+  evt.preventDefault()
+  search_bar.value = ''
+  search_bar.setAttribute('style', 'visibility : hidden')
+  back_button.setAttribute('style', 'visibility:hidden')
+  myFunction()
 }
-search_bar.addEventListener("keyup",
-   function(event) {
-      if (!event) {
-         var event = window.event;
-      }
-         event.preventDefault();
-      if (event.keyCode == 13){
-         myFunction()
-      }
-   }, false)
+search_bar.addEventListener(
+  'keyup',
+  function (event) {
+    if (!event) {
+      var event = window.event
+    }
+    event.preventDefault()
+    if (event.keyCode == 13) {
+      myFunction()
+    }
+  },
+  false,
+)
 
 window.onload = async function () {
   // execute your functions here to make sure they run as soon as the page loads
 
-  getAllMovies()
+  await getAllMovies()
 }
-document.getElementById('load-more-movies-btn').addEventListener('click', LoadMore)
+document
+  .getElementById('load-more-movies-btn')
+  .addEventListener('click', LoadMore)
+function display(responseData) {
+  if (responseData) {
+    responseData.results.forEach((element) => {
+      var image_path = ''
+
+      if ('poster_path' in element) {
+        image_path = element.poster_path
+      }
+      var id = element.id
+
+      const title = element.title
+      const votes = element.vote_average
+
+      grid_cont.innerHTML += `
+      <div class= "movie-card" id="${id}" onclick = "getMovieDetails(${id})">
+      <img class= "movie-poster" src= "https://image.tmdb.org/t/p/w500/${image_path}"/>
+      <div class = "rating">
+      <div class= "star">⭐</div>
+      <div class= "movie-votes"> ${votes}</div>
+      </div>
+      <div class="movie-title">${title}</div>
+      </div>
+      `
+    })
+  }
+}
+async function getMovieDetails(id) {
+  const apiUrl = `https://api.themoviedb.org/3/movie/${id}?api_key=${api_key}`
+
+  let response = await fetch(apiUrl)
+
+  let responseData = await response.json()
+
+  var element = document.querySelector('.demo-popup-wrapper')
+  console.log(element)
+
+  if (element != null)
+  {
+    console.log("nullooo")
+    element.parentNode.removeChild(element)
+  }
+
+  document.querySelector('.navigation').insertAdjacentHTML('beforebegin',
+  `<div class="demo-popup-wrapper" >
+  <div class="demo-popup">
+    <div class="popupcontrols">
+     <span id="closeup">❌</span>
+    </div>
+  <div class="details">
+  <div class="popup-title">Title: ${responseData.title}</div>
+  <div class="popup-star">Rating ⭐: ${responseData.vote_average} </div>
+  <div class="popup-release_Info">Release Info: ${responseData.release_date}</div>
+  <div class="overview"> Overview: ${responseData.overview}</div>
+  <div class="popup-tagline"> Tagline: ${responseData.tagline}</div>
+  </div>
+  </div>
+ 
+  </div>
+
+  `
+  );
+  const popupclose = document.getElementById("closeup").addEventListener("click", closefunc)
+  
+  function closefunc()
+  {
+    var element = document.querySelector('.demo-popup-wrapper')
+     element.parentNode.removeChild(element)
+  }
+}
