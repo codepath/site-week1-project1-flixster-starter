8627c706fccf514ebb374cbfbeb4b9f018fb40a7
Kimberly Truong <kimberlytruong@fb.com>
2022-06-16 17:15:08 -0700

    Finished all expected functionality and graphics

--- script.js
+++ script.js
@@ -1,30 +1,38 @@
 const API_KEY = "27501126c89468d2681cf1346085ad86"
 const BASE_URL = "https://image.tmdb.org/t/p/w500"
 var page = 1
+var search_page = 1
 var column = 1
+var search = ''
 
-const movieResultsElement = document.getElementById("movie-results")
+const movieResultsElement = document.getElementById("movies-grid")
+const searchElement = document.getElementById("search")
+const inputElement = document.getElementById('search-input')
+const loadMoreElement = document.getElementById("load-more-movies-btn")
+const exitSearchElement = document.getElementById("close-search-btn")
 
 async function fetchMovies(){
-    //event.preventDefault()
-
-    const response = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&page=${page}`)
-    const results = await response.json()
-
-    page++
-
-    displayMovies(results["results"])
+    if (search != ''){
+        const response = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&page=${search_page}&include_adult=false&query=${search}`)
+        search_page++
+        const results = await response.json()
+        displayMovies(results["results"])
+    }
+    else{
+        const response = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&page=${page}`)
+        page++
+        const results = await response.json()
+        displayMovies(results["results"])
+    }
 }
 
 function insertMovie (title, src, votes){
-    // insert start icon after image
-    // <i class="fa-solid fa-star"></i>
     movieResultsElement.innerHTML += 
-    `<div class = "movie-box column-${column}">
-        <img src="${src}" class = "movie-img">
+    `<div class = "movie-card column-${column}">
+        <img src="${src}" class = "movie-poster">
         
         <div class = "movie-text">
-            <p class = "votes"><i class="fa-solid fa-star"></i>${votes}</p>
+            <p class = "movie-votes"><i class="fa-solid fa-star"></i>${votes}</p>
             <h3 class = "movie-title">${title}</h3>
         </div>
     </div>
@@ -44,14 +52,60 @@ function displayMovies(movies) {
     var votes;
 
     movies.forEach(movie => {
-        title = movie.title
-        posterImgSrc = BASE_URL + movie.poster_path
-        votes = movie.vote_average
-        
-        insertMovie(title, posterImgSrc, votes)
+
+        if (movie.poster_path != null){
+            title = movie.title
+            posterImgSrc = BASE_URL + movie.poster_path
+            votes = movie.vote_average
+            
+            insertMovie(title, posterImgSrc, votes)
+        }
     })
 }
 
-window.onload = function () {
+function loadMore(event){
+    event.preventDefault()
+
     fetchMovies()
 }
+
+async function searchMovies(event){
+    event.preventDefault()
+
+    if (search != ''){
+        search_page = 1
+    }
+
+    page = 1
+
+    movieResultsElement.innerHTML = ''
+
+    const response = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&page=${search_page}&include_adult=false&query=${inputElement.value}`)
+    const results = await response.json()
+
+    search = inputElement.value
+    inputElement.value = ''
+
+    displayMovies(results["results"])
+}
+
+function clearSearch(){
+    if (search != '' || page != 1){
+        page = 1
+        search_page = 1
+        search = ''
+        movieResultsElement.innerHTML = ''
+        fetchMovies()
+    }
+
+}
+
+window.onload = function () {
+    searchElement.addEventListener('submit', searchMovies)
+    loadMoreElement.addEventListener('click', loadMore)
+    exitSearchElement.addEventListener('click', clearSearch)
+    fetchMovies()
+}
+
+// need to reset search_page
+// make sure there's 4 on each row
