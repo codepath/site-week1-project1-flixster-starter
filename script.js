e96b657474582d5c1f455d62cc049fa9e12bbec5
Nara Macias <nmacias@nmacias-ltmujk4.internal.salesforce.com>
2023-06-07 16:21:14 -0700

    dynamic implemented in js

--- script.js
+++ script.js
@@ -73,3 +73,37 @@ let fakeMoviesAPI = {
     "total_results": 1951
 }
 let firstMovie = fakeMoviesAPI.results[0]
+console.log(firstMovie)
+// create star
+
+function generateCards(movieObject){
+    let star = document.createElement('span');
+    star.classList.add('star');
+    let starContent = document.createTextNode('⭐️');
+    star.appendChild(starContent);
+    // document.body.appendChild(star);
+
+
+    //create rating
+    let rating = document.createElement('span');
+    let ratingContent = document.createTextNode(movieObject.vote_average);
+    star.classList.add('rating');
+    star.appendChild(ratingContent);
+    // document.body.appendChild(rating);
+
+    //create average 
+    let averageContainer = document.createElement('div');
+    star.classList.add('average');
+    averageContainer.appendChild(star);
+    averageContainer.appendChild(rating);
+    document.body.appendChild(averageContainer);
+
+    let image = document.createElement('img');
+    image.src = "https://image.tmdb.org/t/p/w342"+movieObject.poster_path;
+    document.body.insertBefore(image, averageContainer);
+
+
+}
+
+console.log(firstMovie)
+generateCards(firstMovie);
