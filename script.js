fcea5591dc51d656996d4c3d92ac84cb6a0d1db6
Nara Macias <nmacias@nmacias-ltmujk4.internal.salesforce.com>
2023-06-07 16:28:35 -0700

    added average container

--- script.js
+++ script.js
@@ -102,8 +102,13 @@ function generateCards(movieObject){
     image.src = "https://image.tmdb.org/t/p/w342"+movieObject.poster_path;
     document.body.insertBefore(image, averageContainer);
 
+    let name = document.createElement('div');
+    name.classList.add('name');
+    name.innerText = movieObject.original_title;
+    document.body.insertBefore(name, averageContainer.nextSibling);
+
 
 }
 
-console.log(firstMovie)
+
 generateCards(firstMovie);
