dbd14a52957e797239daeec0997213dfe0ac8446
Cesar Monagas Romero <cmonagasromero@cmonaga-ltmyrcq.internal.salesforce.com>
2023-06-07 16:15:07 -0700

    adding image

--- script.js
+++ script.js
@@ -76,5 +76,34 @@ let fakeMoviesAPI = {
     }
 
 let firstMovie = fakeMoviesAPI.results[0] 
-console.log(fakeMoviesAPI.results[0])
+console.log(firstMovie)
+
+function generateCards(movieObject){
+    // create star
+    let star = document.createElement('span');
+    star.classList.add('star');
+    let starContent = document.createTextNode('⭐️');
+    star.appendChild(starContent);
+
+    // create rating
+    let rating = document.createElement('span');
+    let ratingContent = document.createTextNode(movieObject.vote_average);
+    rating.classList.add('rating');
+    rating.appendChild(ratingContent);
+
+    // create average container 
+    let averageContainer = document.createElement('div');
+    averageContainer.appendChild(star);
+    averageContainer.appendChild(rating);
+    averageContainer.classList.add('average');
+    document.body.appendChild(averageContainer);
+
+    // create image
+    let image = document.createElement('img');
+    image.src="https://image.tmdb.org/t/p/w342" + movieObject.poster_path;
+    document.body.insertBefore(image,averageContainer);
+
+}
+
+generateCards(firstMovie);
 
