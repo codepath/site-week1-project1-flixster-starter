12045ac687021a0efcb92281913caadccd33f792
José Inés Martínez <martinez.jose@martine-ltmmvya.internal.salesforce.com>
2023-06-08 14:48:19 -0700

    Added TMBD API and improved website functionality/layout.

--- script.js
+++ script.js
@@ -1,35 +1,46 @@
-// Start by displaying the initial movies from const movie_json, build an "Add more" function by clicking button later
-
-// Store the movies JSON as a const, use JSON.parse method to construct the original object
-
-const txtJSON = '{ "dates": { "maximum": "2023-06-05", "minimum": "2023-04-18" }, "page": 1, "results": [ { "adult": false, "backdrop_path": "/9n2tJBplPbgR2ca05hS5CKXwP2c.jpg", "genre_ids": [ 16, 10751, 12, 14, 35 ], "id": 502356, "original_language": "en", "original_title": "The Super Mario Bros. Movie", "overview": "While working underground to fix a water main, Brooklyn plumbers—and brothers—Mario and Luigi are transported down a mysterious pipe and wander into a magical new world. But when the brothers are separated, Mario embarks on an epic quest to find Luigi.", "popularity": 3392.2, "poster_path": "/qNBAXBIQlnOThrVvA6mA2B5ggV6.jpg", "release_date": "2023-04-05", "title": "The Super Mario Bros. Movie", "video": false, "vote_average": 7.8, "vote_count": 4327 }, { "adult": false, "backdrop_path": "/2I5eBh98Q4aPq8WdQrHdTC8ARhY.jpg", "genre_ids": [ 28, 12, 16, 878 ], "id": 569094, "original_language": "en", "original_title": "Spider-Man: Across the Spider-Verse", "overview": "After reuniting with Gwen Stacy, Brooklyn’s full-time, friendly neighborhood Spider-Man is catapulted across the Multiverse, where he encounters the Spider Society, a team of Spider-People charged with protecting the Multiverse’s very existence. But when the heroes clash on how to handle a new threat, Miles finds himself pitted against the other Spiders and must set out on his own to save those he loves most.", "popularity": 2921.844, "poster_path": "/8Vt6mWEReuy4Of61Lnj5Xj704m8.jpg", "release_date": "2023-05-31", "title": "Spider-Man: Across the Spider-Verse", "video": false, "vote_average": 8.8, "vote_count": 739 }, { "adult": false, "backdrop_path": "/4t0oBFrJyweYPt0hocW6RUa0b6H.jpg", "genre_ids": [ 28, 80, 53 ], "id": 385687, "original_language": "en", "original_title": "Fast X", "overview": "Over many missions and against impossible odds, Dom Toretto and his family have outsmarted, out-nerved and outdriven every foe in their path. Now, they confront the most lethal opponent they\'ve ever faced: A terrifying threat emerging from the shadows of the past who\'s fueled by blood revenge, and who is determined to shatter this family and destroy everything—and everyone—that Dom loves, forever.", "popularity": 2334.66, "poster_path": "/1E5baAaEse26fej7uHcjOgEE2t2.jpg", "release_date": "2023-05-17", "title": "Fast X", "video": false, "vote_average": 7.1, "vote_count": 854 } ], "total_pages": 98, "total_results": 1951 }';
-const request_obj = JSON.parse(txtJSON);
-
-/* 
-Previous JSON is an object with the following attributes:
-@dates // Movie date range
-@page // Current page of movies in given date range
-@results // (List?) of movies fitting previous attributes
-
-On top of that, a movie is also an object with its own attributes:
-@adult, @id, @original_language, ..., @vote_count
-
-TO-DO: 
-Pretend request was made with given attributes (@date and @page). @results is given.
-Extract individual movies from list of movies, and insert them as children into the div 'movie-container' parent element.
-Display the movie posters as images when appended, and give respective changes when adding attributes such as title, year, and such.
-*/
-
-// Find 'movie-container' div (parent node)
-const movieContainerDiv = document.querySelector(".movie-container");
-
-// Figure out a way to fetch an image and create an image element to append to 'movie-container'
-for (const movie of request_obj.results) {
-    let moviePoster = document.createElement('img');
-    moviePoster.className = "movie-poster";
-    const moviePosterURL = "https://image.tmdb.org/t/p/w600_and_h900_bestv2" + movie.poster_path;
-    moviePoster.src = moviePosterURL;
-    movieContainerDiv.appendChild(moviePoster);
+async function fetchMoviesJSON() {
+    const options = {
+        method: 'GET',
+        headers: {
+        accept: 'application/json',
+        Authorization: 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxN2ZkNjVlOGVhNTQ0MDJkOTVjODljMzg0OWE3YzA2YiIsInN1YiI6IjY0ODIxZDYyYmYzMWYyMDBlM2ZiZWI1ZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.KdnfglABbjrMPqqWICxofwlpa9nt9TRC9GRSbupnvPI'
+        }
+    };
+    const res = await fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1', options);
+    const data = await res.json();
+    return data;
 }
 
-// Append movie elements (objects) to 'movie-container' div (parent node)
+function displayNowPlayingMovies() {
+    // Call async function and obtain the array of 'Now playing' movies by checking data.results
+    fetchMoviesJSON().then((data) => {
+        // Find 'movie-container' div (parent node) using querySelector method
+        const generalMovieCont = document.querySelector(".movie-container");
+        // Fetch an image and create an image element to append to 'movie-container'
+        // Create individual movie container and append to general movie container
+        // By appending, add attributes/classes to the individual movie container
+        for (const movie of data.results) {
+            // Appending individual movie container to general container
+            const indivMovieCont = document.createElement('div');
+            generalMovieCont.appendChild(indivMovieCont);
+            // Movie Poster
+            let moviePoster = document.createElement('img');
+            moviePoster.className = "movie-poster"
+            const moviePosterImg = "https://image.tmdb.org/t/p/w342" + movie.poster_path;
+            moviePoster.src = moviePosterImg;
+            indivMovieCont.appendChild(moviePoster);
+            // Title and Rating
+            let titleRate = document.createElement('h3');
+            titleRate.className = "movie-title-rating";
+            const titleRateTxt = movie.original_title + " - ⭐ " + movie.vote_average;
+            titleRate.innerText = titleRateTxt;
+            indivMovieCont.appendChild(titleRate);
+        }
+    });
+}
+
+function init() {
+    displayNowPlayingMovies();
+}
+
+init();
