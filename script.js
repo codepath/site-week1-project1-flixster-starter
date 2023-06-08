5626822207187fb8ed1cc938dd7a5158de700810
Ana Camba Gomes <acamba@acamba-ltmya7c.internal.salesforce.com>
2023-06-08 12:39:22 -0700

    movies showing with fetch

--- script.js
+++ script.js
@@ -1,83 +1,87 @@
-let fakeMoviesAPI = {
-    "dates": {
-        "maximum": "2023-06-05",
-        "minimum": "2023-04-18"
-    },
-    "page": 1,
-    "results": [
-        {
-            "adult": false,
-            "backdrop_path": "/9n2tJBplPbgR2ca05hS5CKXwP2c.jpg",
-            "genre_ids": [
-                16,
-                10751,
-                12,
-                14,
-                35
-            ],
-            "id": 502356,
-            "original_language": "en",
-            "original_title": "The Super Mario Bros. Movie",
-            "overview": "While working underground to fix a water main, Brooklyn plumbers—and brothers—Mario and Luigi are transported down a mysterious pipe and wander into a magical new world. But when the brothers are separated, Mario embarks on an epic quest to find Luigi.",
-            "popularity": 3392.2,
-            "poster_path": "/qNBAXBIQlnOThrVvA6mA2B5ggV6.jpg",
-            "release_date": "2023-04-05",
-            "title": "The Super Mario Bros. Movie",
-            "video": false,
-            "vote_average": 7.8,
-            "vote_count": 4327
-        },
-        {
-            "adult": false,
-            "backdrop_path": "/2I5eBh98Q4aPq8WdQrHdTC8ARhY.jpg",
-            "genre_ids": [
-                28,
-                12,
-                16,
-                878
-            ],
-            "id": 569094,
-            "original_language": "en",
-            "original_title": "Spider-Man: Across the Spider-Verse",
-            "overview": "After reuniting with Gwen Stacy, Brooklyn’s full-time, friendly neighborhood Spider-Man is catapulted across the Multiverse, where he encounters the Spider Society, a team of Spider-People charged with protecting the Multiverse’s very existence. But when the heroes clash on how to handle a new threat, Miles finds himself pitted against the other Spiders and must set out on his own to save those he loves most.",
-            "popularity": 2921.844,
-            "poster_path": "/8Vt6mWEReuy4Of61Lnj5Xj704m8.jpg",
-            "release_date": "2023-05-31",
-            "title": "Spider-Man: Across the Spider-Verse",
-            "video": false,
-            "vote_average": 8.8,
-            "vote_count": 739
-        },
-        {
-            "adult": false,
-            "backdrop_path": "/4t0oBFrJyweYPt0hocW6RUa0b6H.jpg",
-            "genre_ids": [
-                28,
-                80,
-                53
-            ],
-            "id": 385687,
-            "original_language": "en",
-            "original_title": "Fast X",
-            "overview": "Over many missions and against impossible odds, Dom Toretto and his family have outsmarted, out-nerved and outdriven every foe in their path. Now, they confront the most lethal opponent they've ever faced: A terrifying threat emerging from the shadows of the past who's fueled by blood revenge, and who is determined to shatter this family and destroy everything—and everyone—that Dom loves, forever.",
-            "popularity": 2334.66,
-            "poster_path": "/1E5baAaEse26fej7uHcjOgEE2t2.jpg",
-            "release_date": "2023-05-17",
-            "title": "Fast X",
-            "video": false,
-            "vote_average": 7.1,
-            "vote_count": 854
-        },
-    ],
-    "total_pages": 98,
-    "total_results": 1951
-}
+// let fakeMoviesAPI = {
+//     "dates": {
+//         "maximum": "2023-06-05",
+//         "minimum": "2023-04-18"
+//     },
+//     "page": 1,
+//     "results": [
+//         {
+//             "adult": false,
+//             "backdrop_path": "/9n2tJBplPbgR2ca05hS5CKXwP2c.jpg",
+//             "genre_ids": [
+//                 16,
+//                 10751,
+//                 12,
+//                 14,
+//                 35
+//             ],
+//             "id": 502356,
+//             "original_language": "en",
+//             "original_title": "The Super Mario Bros. Movie",
+//             "overview": "While working underground to fix a water main, Brooklyn plumbers—and brothers—Mario and Luigi are transported down a mysterious pipe and wander into a magical new world. But when the brothers are separated, Mario embarks on an epic quest to find Luigi.",
+//             "popularity": 3392.2,
+//             "poster_path": "/qNBAXBIQlnOThrVvA6mA2B5ggV6.jpg",
+//             "release_date": "2023-04-05",
+//             "title": "The Super Mario Bros. Movie",
+//             "video": false,
+//             "vote_average": 7.8,
+//             "vote_count": 4327
+//         },
+//         {
+//             "adult": false,
+//             "backdrop_path": "/2I5eBh98Q4aPq8WdQrHdTC8ARhY.jpg",
+//             "genre_ids": [
+//                 28,
+//                 12,
+//                 16,
+//                 878
+//             ],
+//             "id": 569094,
+//             "original_language": "en",
+//             "original_title": "Spider-Man: Across the Spider-Verse",
+//             "overview": "After reuniting with Gwen Stacy, Brooklyn’s full-time, friendly neighborhood Spider-Man is catapulted across the Multiverse, where he encounters the Spider Society, a team of Spider-People charged with protecting the Multiverse’s very existence. But when the heroes clash on how to handle a new threat, Miles finds himself pitted against the other Spiders and must set out on his own to save those he loves most.",
+//             "popularity": 2921.844,
+//             "poster_path": "/8Vt6mWEReuy4Of61Lnj5Xj704m8.jpg",
+//             "release_date": "2023-05-31",
+//             "title": "Spider-Man: Across the Spider-Verse",
+//             "video": false,
+//             "vote_average": 8.8,
+//             "vote_count": 739
+//         },
+//         {
+//             "adult": false,
+//             "backdrop_path": "/4t0oBFrJyweYPt0hocW6RUa0b6H.jpg",
+//             "genre_ids": [
+//                 28,
+//                 80,
+//                 53
+//             ],
+//             "id": 385687,
+//             "original_language": "en",
+//             "original_title": "Fast X",
+//             "overview": "Over many missions and against impossible odds, Dom Toretto and his family have outsmarted, out-nerved and outdriven every foe in their path. Now, they confront the most lethal opponent they've ever faced: A terrifying threat emerging from the shadows of the past who's fueled by blood revenge, and who is determined to shatter this family and destroy everything—and everyone—that Dom loves, forever.",
+//             "popularity": 2334.66,
+//             "poster_path": "/1E5baAaEse26fej7uHcjOgEE2t2.jpg",
+//             "release_date": "2023-05-17",
+//             "title": "Fast X",
+//             "video": false,
+//             "vote_average": 7.1,
+//             "vote_count": 854
+//         },
+//     ],
+//     "total_pages": 98,
+//     "total_results": 1951
+// }
 
-let firstMovie = fakeMoviesAPI.results[0] // gets access to all properties of first movie in array
-let secondMovie = fakeMoviesAPI.results[1]
-let thirdMovie = fakeMoviesAPI.results[2]
+// async function fn() {
+//     const res = await fetch(url)
+//     const data = await response.json()
+//     console.log(data)
+//   }
+  
+//   fn()
 
-console.log(firstMovie)
+
 
 function generateCards(movieObject){ // function that will generate the movie cards in our mini netflix
     // create star
@@ -123,9 +127,10 @@ function generateCards(movieObject){ // function that will generate the movie ca
     document.body.appendChild(movie)
     movie.classList.add('movie')
 
-
+    return movie;
 
 }
+
 
 // let firstCard = generateCards(firstMovie);
 // let secondCard = generateCards(secondMovie);
@@ -135,21 +140,29 @@ function generateCards(movieObject){ // function that will generate the movie ca
 // generateCards(secondMovie);
 // generateCards(thirdMovie);
 
-for (let i = 0; i<3; i++){
+// for (let i = 0; i<3; i++){
 
-    generateCards(fakeMoviesAPI.results[i]);
+//     generateCards(fakeMoviesAPI.results[i]);
 
-}
+// }
+//console.log(generateCards(fakeMoviesAPI.results[0]))
 
 // // create movie container
 
-// let movieContainer = document.createElement('section');
-// movieContainer.classList.add('movieContainer')
-// for (let i = 0; i<3; i++){
+let movieContainer = document.createElement('section');
+movieContainer.classList.add('movieContainer')
 
-//     movieContainer.appendChild(generateCards(fakeMoviesAPI.results[i])) ;
+fetch("https://api.themoviedb.org/3/discover/movie?api_key=4026d59afea2b4fa4ab32088708c56c1").then((response) => response.json()).then((data) => {
+    console.log(data);
+    for (let i = 0; i<data.results.length; i++){
 
-// }
-// document.body.appe
+        movieContainer.appendChild(generateCards(data.results[i])) ;
+        //console.log(fakeMoviesAPI.results[i]);
+    
+    }
+})
+
+
+document.body.appendChild(movieContainer)
 
 
