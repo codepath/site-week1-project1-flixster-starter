05e6d11523a2e9dfd53bf4a3207e79b9061e31b4
Oluwapelumi Tayo Orisadare <ptayoorisadare@ptayoor-ltm7f2y.internal.salesforce.com>
2023-06-09 12:05:44 -0700

    adding search and load more feature. Fully functional

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
 const API_KEY  = "ec19ad523e949b880b38dce4fe91d6fe"
 let url = `https://api.themoviedb.org/3/discover/movie?api_key=ec19ad523e949b880b38dce4fe91d6fe`
+let search_url = 'https://api.themoviedb.org/3/search/movie?query='
 
-async function populateMovieSpace() {
+let form = document.querySelector('#search-form')
+let search = document.querySelector('#search')
+let movieContainer = document.querySelector('#movie-container')
+let loadMore = document.querySelector('#load-more') 
+
+populateMovieSpace(url)
+
+async function populateMovieSpace(url) {
     const response = await fetch(url)
     const data = await response.json()
+    console.log(data.results)
     data.results.forEach( (movie) => {
         generateCard(movie);
     })
-    
 }
-
-
-let movieContainer = document.querySelector('#movie-container')
-// movieContainer.classList.add("movieContainer")
-// document.body.appendChild(movieContainer)
 
 function generateCard(movieAPI){
     //create star
@@ -36,7 +39,13 @@ function generateCard(movieAPI){
 
     //create image
     let image = document.createElement('img')
-    image.src = "https://image.tmdb.org/t/p/w342" + movieAPI.poster_path
+    if(movieAPI.poster_path !== null){
+        image.src = "https://image.tmdb.org/t/p/w342" + movieAPI.poster_path
+    }
+    else{
+       image.src = "https://www.dreamstime.com/page-error-cartoon-style-template-reports-page-was-not-found-page-error-cartoon-style-bear-image102988168"
+    }
+    image.alt = movieAPI.original_title
 
     //create movie-name
     let movieName = document.createElement('div')
@@ -52,4 +61,24 @@ function generateCard(movieAPI){
     movieContainer.appendChild(movie)
 }
 
-populateMovieSpace()
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    searchValue = search.value
+    console.log(searchValue)
+    if (searchValue && searchValue !==''){
+        movieContainer.innerHTML = ''
+        populateMovieSpace(search_url+searchValue+"&api_key="+API_KEY)
+        search.value = ''
+    }
+    else{
+        window.location.reload()
+    }
+})
+
+let pageNum = 2
+loadMore.addEventListener('click', (e) => {
+    populateMovieSpace(url+"&page="+pageNum.toString())
+    pageNum += 1
+})
+
+
