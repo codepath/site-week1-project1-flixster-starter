7d1bcbc4214e90dcdbc25105da1f4a258dc5632d
Nara Macias <nmacias@nmacias-ltmujk4.internal.salesforce.com>
2023-06-09 22:29:56 -0700

    final

--- script.js
+++ script.js
@@ -1,85 +1,11 @@
-let fakeMoviesAPI = {
-    "dates": {
-        "maximum": "2023-06-05",
-        "minimum": "2023-04-18"
-    },
-    "page": 1,
-    "results": [
-        {
-            "adult": false,
-            "backdrop_path": "/9n2tJBplPbgR2ca05hS5CKXwP2c.jpg",
-            "genre_ids": [
-                16,
-                10751,
-                12,
-                14,
-                35
-            ],
-            "id": 502356,
-            "original_language": "en",
-            "original_title": "The Super Mario Bros. Movie",
-            "overview": "While working underground to fix a water main, Brooklyn plumbers—and brothers—Mario and Luigi are transported down a mysterious pipe and wander into a magical new world. But when the brothers are separated, Mario embarks on an epic quest to find Luigi.",
-            "popularity": 3392.2,
-            "poster_path": "/qNBAXBIQlnOThrVvA6mA2B5ggV6.jpg",
-            "release_date": "2023-04-05",
-            "title": "The Super Mario Bros. Movie",
-            "video": false,
-            "vote_average": 7.8,
-            "vote_count": 4327
-        },
-        {
-            "adult": false,
-            "backdrop_path": "/2I5eBh98Q4aPq8WdQrHdTC8ARhY.jpg",
-            "genre_ids": [
-                28,
-                12,
-                16,
-                878
-            ],
-            "id": 569094,
-            "original_language": "en",
-            "original_title": "Spider-Man: Across the Spider-Verse",
-            "overview": "After reuniting with Gwen Stacy, Brooklyn’s full-time, friendly neighborhood Spider-Man is catapulted across the Multiverse, where he encounters the Spider Society, a team of Spider-People charged with protecting the Multiverse’s very existence. But when the heroes clash on how to handle a new threat, Miles finds himself pitted against the other Spiders and must set out on his own to save those he loves most.",
-            "popularity": 2921.844,
-            "poster_path": "/8Vt6mWEReuy4Of61Lnj5Xj704m8.jpg",
-            "release_date": "2023-05-31",
-            "title": "Spider-Man: Across the Spider-Verse",
-            "video": false,
-            "vote_average": 8.8,
-            "vote_count": 739
-        },
-        {
-            "adult": false,
-            "backdrop_path": "/4t0oBFrJyweYPt0hocW6RUa0b6H.jpg",
-            "genre_ids": [
-                28,
-                80,
-                53
-            ],
-            "id": 385687,
-            "original_language": "en",
-            "original_title": "Fast X",
-            "overview": "Over many missions and against impossible odds, Dom Toretto and his family have outsmarted, out-nerved and outdriven every foe in their path. Now, they confront the most lethal opponent they've ever faced: A terrifying threat emerging from the shadows of the past who's fueled by blood revenge, and who is determined to shatter this family and destroy everything—and everyone—that Dom loves, forever.",
-            "popularity": 2334.66,
-            "poster_path": "/1E5baAaEse26fej7uHcjOgEE2t2.jpg",
-            "release_date": "2023-05-17",
-            "title": "Fast X",
-            "video": false,
-            "vote_average": 7.1,
-            "vote_count": 854
-        },
-    ],
-    "total_pages": 98,
-    "total_results": 1951
-}
-let firstMovie = fakeMoviesAPI.results[0]
-console.log(firstMovie)
-// create star
+
+const searchButton = document.querySelector('#search-button');
+let searchBar = document.querySelector('#search-input');
 
 function generateCards(movieObject){
     let star = document.createElement('span');
     star.classList.add('star');
-    let starContent = document.createTextNode('⭐️');
+    let starContent = document.createTextNode('⭐️ ');
     star.appendChild(starContent);
     // document.body.appendChild(star);
 
@@ -96,19 +22,113 @@ function generateCards(movieObject){
     star.classList.add('average');
     averageContainer.appendChild(star);
     averageContainer.appendChild(rating);
-    document.body.appendChild(averageContainer);
+    // document.body.appendChild(averageContainer);
 
+    // create image 
     let image = document.createElement('img');
-    image.src = "https://image.tmdb.org/t/p/w342"+movieObject.poster_path;
-    document.body.insertBefore(image, averageContainer);
+    let defaultImage = "https://motivatevalmorgan.com/wp-content/uploads/2016/06/default-movie.jpg"
+    image.src = movieObject.poster_path ? "https://image.tmdb.org/t/p/w342" + movieObject.poster_path : defaultImage
+    image.className = 'movie-poster'; 
+    image.alt = movieObject.original_title; 
 
+    //create name section
     let name = document.createElement('div');
     name.classList.add('name');
     name.innerText = movieObject.original_title;
-    document.body.insertBefore(name, averageContainer.nextSibling);
+    // document.body.insertBefore(name, averageContainer.nextSibling);
 
+    //create movie section
+    let movie = document.createElement('section');
+    name.classList.add('movie');
+    movie.appendChild(image);
+    movie.appendChild(averageContainer);
+    movie.appendChild(name);
+    document.body.appendChild(movie);
+    
+    return movie;
 
 }
 
+const  movieContainer = document.querySelector('#movie-grid') 
+// const loadMoreButton = document.querySelector('#load-more-button')
 
-generateCards(firstMovie);
+function display (page){
+fetch( `https://api.themoviedb.org/3/discover/movie?api_key=57b58a649f61e3a9ea0cf603c932ffec&page=${page} `)
+  .then((response) => response.json())
+  .then((data) => {
+    for (let i = 0; i < data.results.length; i++) {
+      movieContainer.appendChild(generateCards(data.results[i]));
+    }
+  });
+
+
+
+}
+//////////////////////
+
+let pageNumber = 1; 
+let button = document.getElementById('load-more-movies-btn');
+
+button.addEventListener('click',nextPage);
+
+let searchMade = false;
+let searchTerm = '';
+
+function nextPage(){
+    pageNumber += 1;
+    if (searchMade) {
+        search(searchTerm,pageNumber)
+    } else {
+        display(pageNumber)
+    }
+    
+
+}
+function search(keyword, page = 1){
+    searchMade = true;
+    searchTerm = keyword;
+    const options = {
+        method: 'GET',
+        headers: {
+          accept: 'application/json',
+          Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1N2I1OGE2NDlmNjFlM2E5ZWEwY2Y2MDNjOTMyZmZlYyIsInN1YiI6IjY0ODIwNjAyZDJiMjA5MDEwYzE3Y2Y0MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.o-USUsNsrlcEm9NNKSAJ7022AYC37EhewmyoWt2Xhwg'
+        }
+      };
+      
+      fetch(`https://api.themoviedb.org/3/search/movie?query=${keyword}&include_adult=false&language=en-US&page=${page}`, options)        .then(response => response.json())
+        .then((data) => {
+            console.log(data)
+            for (let i = 0; i < data.results.length; i++) {
+                movieContainer.appendChild(generateCards(data.results[i]));
+                console.log(data.results[i])
+            }
+
+})
+}
+searchButton.addEventListener('click', function(event){
+    event.preventDefault()
+    movieContainer.innerHTML = ''
+    let searchValue = searchBar.value
+    search(searchValue);
+    console.log(searchBar.value)
+});
+
+const closeSearchButton = document.getElementById('close-search-btn');
+
+closeSearchButton.addEventListener('click', function () {
+    movieContainer.innerHTML = ''; 
+    pageNumber = 1; 
+    display(pageNumber); 
+    searchBar.value = ''; 
+    searchMade = false; 
+});
+
+
+
+
+
+window.onload = function(){
+    display(1);
+
+}
+
