4210391322963c5847fc80f513fba586a6ff2d60
Vanessa Ibe <vibe@vibe-ltmtd24.internal.salesforce.com>
2023-06-09 21:55:30 -0700

    completed final

--- README.md
+++ README.md
@@ -12,25 +12,26 @@ Deployed Application (optional): [Flixster Deployed Site](ADD_LINK_HERE)
 
 #### Core Features
 
-- [ ] User can view a list of current movies from The Movie Database API as a grid view
+- [x] User can view a list of current movies from The Movie Database API as a grid view
   - The grid element should have an id of `movies-grid`
   - Each movie wrapper element should have a class of `movie-card`
-- [ ] For each movie displayed, user can see the following details:
+- [x] For each movie displayed, user can see the following details:
   - Title - the element should have a class of `movie-title`
   - Image - the `img` element should have a class of `movie-poster`
-  - Votes - the element should have a class of `movie-votes`
-- [ ] User can load more current movies by clicking a button at the bottom of the list
+  -
+   Votes - the element should have a class of `movie-votes`
+- [x] User can load more current movies by clicking a button at the bottom of the list
   - The button should have an id of `load-more-movies-btn`.
   - When clicked, the page should not refresh.
   - New movies should simply be added to the bottom
-- [ ] Allow users to search for movies and display them in a grid view
+- [x] Allow users to search for movies and display them in a grid view
   - There should be a search input element with an id of `search-input`
   - Users should be able to type into the input
   - When a user hits 'Enter', it should send a search request to the movies API
   - The results from the search should be displayed on the page
   - There should be a close icon with an id of `close-search-btn` that exits the search, clears results, and shows the current movies displayed previously
-- [ ] Website accounts for basic HTML/CSS accessibility features
-- [ ] Website should be responsive
+- [x] Website accounts for basic HTML/CSS accessibility features
+- [x] Website should be responsive
 
 #### Stretch Features
 
@@ -42,28 +43,24 @@ Deployed Application (optional): [Flixster Deployed Site](ADD_LINK_HERE)
 
 ### Walkthrough Video
 
-`TODO://` Add the embedded URL code to your animated app walkthrough below, `ADD_EMBEDDED_CODE_HERE`. Make sure the video or gif actually renders and animates when viewing this README. (🚫 Remove this paragraph after adding walkthrough video)
-
-`ADD_EMBEDDED_CODE_HERE`
+<a href="https://www.loom.com/share/4f8c061d6b404937bdfe1831fe61a2f3">
+  
+   <img style="max-width:300px;" src="https://cdn.loom.com/sessions/thumbnails/4f8c061d6b404937bdfe1831fe61a2f3-with-play.gif">
+  </a>
 
 ### Reflection
 
 - Did the topics discussed in your labs prepare you to complete the assignment? Be specific, which features in your weekly assignment did you feel unprepared to complete?
-
-Add your response here
+  The topics in my labs prepared me to complete the assignment, especially lab 3 where we also integrated API. I felt unprepared add some stretch features through CSS & animation.
 
 - If you had more time, what would you have done differently? Would you have added additional features? Changed the way your project responded to a particular event, etc.
-  
-Add your response here
+  If I had more time I wouldv'e liked to fiddle with different CSS & animations to portray my website in the best way possible.
 
 - Reflect on your project demo, what went well? Were there things that maybe didn't go as planned? Did you notice something that your peer did that you would like to try next time?
-
-Add your response here
+  I think creating a basic layout went really well, it was fascinating seeing how such a basic thing could improve so much. I think next time i'd like to toggle with the dark-mode feature. It's interesting how that could make such a big difference in terms of accessibility.
 
 ### Open-source libraries used
-
-- Add any links to open-source libraries used in your project.
+-N/A 
 
 ### Shout out
-
-Give a shout out to somebody from your cohort that especially helped you during your project. This can be a fellow peer, instructor, TA, mentor, etc.
+Shout out to Luis for always having my back when i come across a rough patch!
--- script.js
+++ script.js
@@ -1,83 +1,9 @@
-let fakeMoviesAPI = {
-    "dates": {
-        "maximum": "2023-06-05",
-        "minimum": "2023-04-18"
-    },
-    "page": 1,
-    "results": [
-        {
-            "adult": false,
-            "backdrop_path": "/9n2tJBplPbgR2ca05hS5CKXwP2c.jpg",
-            "genre_ids": [
-                16,
-                10751,
-                12,
-                14,
-                35
-            ],
-            "id": 502356,
-            "original_language": "en",
-            "original_title": "The Super Mario Bros. Movie",
-            "overview": "While working underground to fix a water main, Brooklyn plumbers—and brothers—Mario and Luigi are transported down a mysterious pipe and wander into a magical new world. But when the brothers are separated, Mario embarks on an epic quest to find Luigi.",
-            "popularity": 3392.2,
-            "poster_path": "/qNBAXBIQlnOThrVvA6mA2B5ggV6.jpg",
-            "release_date": "2023-04-05",
-            "title": "The Super Mario Bros. Movie",
-            "video": false,
-            "vote_average": 7.8,
-            "vote_count": 4327
-        },
-        {
-            "adult": false,
-            "backdrop_path": "/2I5eBh98Q4aPq8WdQrHdTC8ARhY.jpg",
-            "genre_ids": [
-                28,
-                12,
-                16,
-                878
-            ],
-            "id": 569094,
-            "original_language": "en",
-            "original_title": "Spider-Man: Across the Spider-Verse",
-            "overview": "After reuniting with Gwen Stacy, Brooklyn’s full-time, friendly neighborhood Spider-Man is catapulted across the Multiverse, where he encounters the Spider Society, a team of Spider-People charged with protecting the Multiverse’s very existence. But when the heroes clash on how to handle a new threat, Miles finds himself pitted against the other Spiders and must set out on his own to save those he loves most.",
-            "popularity": 2921.844,
-            "poster_path": "/8Vt6mWEReuy4Of61Lnj5Xj704m8.jpg",
-            "release_date": "2023-05-31",
-            "title": "Spider-Man: Across the Spider-Verse",
-            "video": false,
-            "vote_average": 8.8,
-            "vote_count": 739
-        },
-        {
-            "adult": false,
-            "backdrop_path": "/4t0oBFrJyweYPt0hocW6RUa0b6H.jpg",
-            "genre_ids": [
-                28,
-                80,
-                53
-            ],
-            "id": 385687,
-            "original_language": "en",
-            "original_title": "Fast X",
-            "overview": "Over many missions and against impossible odds, Dom Toretto and his family have outsmarted, out-nerved and outdriven every foe in their path. Now, they confront the most lethal opponent they've ever faced: A terrifying threat emerging from the shadows of the past who's fueled by blood revenge, and who is determined to shatter this family and destroy everything—and everyone—that Dom loves, forever.",
-            "popularity": 2334.66,
-            "poster_path": "/1E5baAaEse26fej7uHcjOgEE2t2.jpg",
-            "release_date": "2023-05-17",
-            "title": "Fast X",
-            "video": false,
-            "vote_average": 7.1,
-            "vote_count": 854
-        },
-    ],
-    "total_pages": 98,
-    "total_results": 1951
-}
 const APIkey = '1e320c36139d1385dc507f9cd8c3890d'
 let page_number = 1
 let query = ""
 let url = 'https://api.themoviedb.org/3/movie/now_playing?page='+  page_number +'&api_key=' + APIkey
 let searchURL ='https://api.themoviedb.org/3/search/movie?query=' + query + '&page='+page_number + '&api_key=' +APIkey
-let movieList = document.querySelector('.movie-list') 
+let movieList = document.getElementById('movies-grid') 
 let searching = false 
 
 async function ApiCall(){
@@ -102,10 +28,11 @@ function generateCards(movieObject) {
     //create rating
     let rating = document.createElement('span');
     let ratingContent = document.createTextNode(movieObject.vote_average);
-    rating.classList.add('rating');
+    rating.classList.add('movie-votes');
     rating.appendChild(ratingContent);
 
     let image = document.createElement('img');
+    image.classList.add("movie-poster")
     image.src = "https://image.tmdb.org/t/p/w342" + movieObject.poster_path;
 
     let name = document.createElement ('div');
@@ -113,7 +40,7 @@ function generateCards(movieObject) {
     name.innerText = movieObject.original_title;
     //create average container
     let averageContainer = document.createElement('div');
-    averageContainer.classList.add('average');
+    averageContainer.classList.add('movie-card');
     averageContainer.appendChild(image);
     averageContainer.appendChild(name)
     averageContainer.appendChild(star);
@@ -122,23 +49,27 @@ function generateCards(movieObject) {
 
 }
 
-document.querySelector("#srch-input").addEventListener("input",(event) =>{
+document.querySelector("#search-input").addEventListener("input",(event) =>{
     query = event.target.value
 })
 
 document.querySelector('#srch-button').addEventListener("click", async ()=> {
-    searchURL ='https://api.themoviedb.org/3/search/movie?query=' + query + '&api_key=' +APIkey
-    searching = true
-    console.log(query)
-    movieList.innerHTML = ""
-    data = await ApiCallSearch()
-    for(let i=0; i< data.results.length; i++){
-        generateCards(data.results[i])
-       }
+    if (query.length>0){
+        event.preventDefault()
+        searchURL ='https://api.themoviedb.org/3/search/movie?query=' + query + '&api_key=' +APIkey
+        searching = true
+        console.log(query)
+        movieList.innerHTML = ""
+        data = await ApiCallSearch()
+        for(let i=0; i< data.results.length; i++){
+            generateCards(data.results[i])
+        }
+    }
+    
 
 })
 
-const searchInput= document.querySelector("#srch-input")
+const searchInput= document.querySelector("#search-input")
 
 searchInput.addEventListener("input",(event) =>{
     query = event.target.value
