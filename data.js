3aa0c3e017704fd2782cd62ef23e2f6c18ea825b
Marley Burrows <mburrows@mburrow-ltm8p9u.internal.salesforce.com>
2023-06-11 22:43:42 -0700

    final save before submission

--- data.js
+++ data.js
@@ -1,179 +1,6 @@
-// let fakeMovies = {
-//     "dates": {
-//         "maximum": "2023-06-05",
-//         "minimum": "2023-04-18"
-//     },
-//     "page": 1,
-//     "results": [
-//         {
-//             "adult": false,
-//             "backdrop_path": "/9n2tJBplPbgR2ca05hS5CKXwP2c.jpg",
-//             "genre_ids": [
-//                 16,
-//                 10751,
-//                 12,
-//                 14,
-//                 35
-//             ],
-//             "id": 502356,
-//             "original_language": "en",
-//             "original_title": "The Super Mario Bros. Movie",
-//             "overview": "While working underground to fix a water main, Brooklyn plumbers—and brothers—Mario and Luigi are transported down a mysterious pipe and wander into a magical new world. But when the brothers are separated, Mario embarks on an epic quest to find Luigi.",
-//             "popularity": 3392.2,
-//             "poster_path": "/qNBAXBIQlnOThrVvA6mA2B5ggV6.jpg",
-//             "release_date": "2023-04-05",
-//             "title": "The Super Mario Bros. Movie",
-//             "video": false,
-//             "vote_average": 7.8,
-//             "vote_count": 4327
-//         },
-//         {
-//             "adult": false,
-//             "backdrop_path": "/2I5eBh98Q4aPq8WdQrHdTC8ARhY.jpg",
-//             "genre_ids": [
-//                 28,
-//                 12,
-//                 16,
-//                 878
-//             ],
-//             "id": 569094,
-//             "original_language": "en",
-//             "original_title": "Spider-Man: Across the Spider-Verse",
-//             "overview": "After reuniting with Gwen Stacy, Brooklyn’s full-time, friendly neighborhood Spider-Man is catapulted across the Multiverse, where he encounters the Spider Society, a team of Spider-People charged with protecting the Multiverse’s very existence. But when the heroes clash on how to handle a new threat, Miles finds himself pitted against the other Spiders and must set out on his own to save those he loves most.",
-//             "popularity": 2921.844,
-//             "poster_path": "/8Vt6mWEReuy4Of61Lnj5Xj704m8.jpg",
-//             "release_date": "2023-05-31",
-//             "title": "Spider-Man: Across the Spider-Verse",
-//             "video": false,
-//             "vote_average": 8.8,
-//             "vote_count": 739
-//         },
-//         {
-//             "adult": false,
-//             "backdrop_path": "/4t0oBFrJyweYPt0hocW6RUa0b6H.jpg",
-//             "genre_ids": [
-//                 28,
-//                 80,
-//                 53
-//             ],
-//             "id": 385687,
-//             "original_language": "en",
-//             "original_title": "Fast X",
-//             "overview": "Over many missions and against impossible odds, Dom Toretto and his family have outsmarted, out-nerved and outdriven every foe in their path. Now, they confront the most lethal opponent they've ever faced: A terrifying threat emerging from the shadows of the past who's fueled by blood revenge, and who is determined to shatter this family and destroy everything—and everyone—that Dom loves, forever.",
-//             "popularity": 2334.66,
-//             "poster_path": "/1E5baAaEse26fej7uHcjOgEE2t2.jpg",
-//             "release_date": "2023-05-17",
-//             "title": "Fast X",
-//             "video": false,
-//             "vote_average": 7.1,
-//             "vote_count": 854
-//         }
-//         ,
-//         {
-//             "adult": false,
-//             "backdrop_path": "/4t0oBFrJyweYPt0hocW6RUa0b6H.jpg",
-//             "genre_ids": [
-//                 28,
-//                 80,
-//                 53
-//             ],
-//             "id": 385687,
-//             "original_language": "en",
-//             "original_title": "Fast X",
-//             "overview": "Over many missions and against impossible odds, Dom Toretto and his family have outsmarted, out-nerved and outdriven every foe in their path. Now, they confront the most lethal opponent they've ever faced: A terrifying threat emerging from the shadows of the past who's fueled by blood revenge, and who is determined to shatter this family and destroy everything—and everyone—that Dom loves, forever.",
-//             "popularity": 2334.66,
-//             "poster_path": "/1E5baAaEse26fej7uHcjOgEE2t2.jpg",
-//             "release_date": "2023-05-17",
-//             "title": "Fast X",
-//             "video": false,
-//             "vote_average": 7.1,
-//             "vote_count": 854
-//         }
-//         ,
-//         {
-//             "adult": false,
-//             "backdrop_path": "/4t0oBFrJyweYPt0hocW6RUa0b6H.jpg",
-//             "genre_ids": [
-//                 28,
-//                 80,
-//                 53
-//             ],
-//             "id": 385687,
-//             "original_language": "en",
-//             "original_title": "Fast X",
-//             "overview": "Over many missions and against impossible odds, Dom Toretto and his family have outsmarted, out-nerved and outdriven every foe in their path. Now, they confront the most lethal opponent they've ever faced: A terrifying threat emerging from the shadows of the past who's fueled by blood revenge, and who is determined to shatter this family and destroy everything—and everyone—that Dom loves, forever.",
-//             "popularity": 2334.66,
-//             "poster_path": "/1E5baAaEse26fej7uHcjOgEE2t2.jpg",
-//             "release_date": "2023-05-17",
-//             "title": "Fast X",
-//             "video": false,
-//             "vote_average": 7.1,
-//             "vote_count": 854
-//         }
-//         ,
-//         {
-//             "adult": false,
-//             "backdrop_path": "/4t0oBFrJyweYPt0hocW6RUa0b6H.jpg",
-//             "genre_ids": [
-//                 28,
-//                 80,
-//                 53
-//             ],
-//             "id": 385687,
-//             "original_language": "en",
-//             "original_title": "Fast X",
-//             "overview": "Over many missions and against impossible odds, Dom Toretto and his family have outsmarted, out-nerved and outdriven every foe in their path. Now, they confront the most lethal opponent they've ever faced: A terrifying threat emerging from the shadows of the past who's fueled by blood revenge, and who is determined to shatter this family and destroy everything—and everyone—that Dom loves, forever.",
-//             "popularity": 2334.66,
-//             "poster_path": "/1E5baAaEse26fej7uHcjOgEE2t2.jpg",
-//             "release_date": "2023-05-17",
-//             "title": "Fast X",
-//             "video": false,
-//             "vote_average": 7.1,
-//             "vote_count": 854
-//         }
-//         ,
-//         {
-//             "adult": false,
-//             "backdrop_path": "/4t0oBFrJyweYPt0hocW6RUa0b6H.jpg",
-//             "genre_ids": [
-//                 28,
-//                 80,
-//                 53
-//             ],
-//             "id": 385687,
-//             "original_language": "en",
-//             "original_title": "Fast X",
-//             "overview": "Over many missions and against impossible odds, Dom Toretto and his family have outsmarted, out-nerved and outdriven every foe in their path. Now, they confront the most lethal opponent they've ever faced: A terrifying threat emerging from the shadows of the past who's fueled by blood revenge, and who is determined to shatter this family and destroy everything—and everyone—that Dom loves, forever.",
-//             "popularity": 2334.66,
-//             "poster_path": "/1E5baAaEse26fej7uHcjOgEE2t2.jpg",
-//             "release_date": "2023-05-17",
-//             "title": "Fast X",
-//             "video": false,
-//             "vote_average": 7.1,
-//             "vote_count": 854
-//         }
-//     ],
-//     "total_pages": 98,
-//     "total_results": 1951
-// }
-
-// console.log(fakeMovies.results[0])
-
-// firstMovie = fakeMovies.results[0];
+const API_KEY = '7a7de07845bd21df37d2fed08dc1f49c'
 
 
-    // const API_URL = 'https://api.themoviedb.org/3/movie/now_playing';
-    const API_KEY = '7a7de07845bd21df37d2fed08dc1f49c'
-
-    // async function fetchMovies() {
-    //     const response = await fetch(API_URL)
-    //     const moviesList = await response.json();
-    //     console.log(moviesList)
-    //     // return moviesList;
-        
-
-
-    
 
 function generateCards(movieObject){
     // creates star
@@ -223,5 +50,32 @@ function generateCards(movieObject){
     })
     .catch(err => console.error(err));
 
-// generateCards(firstMovie);
-// getMovies().results.forEach(generateCards)
+
+    // function search() {
+
+    // }
+
+    let currAllowed = 0
+
+    async function loadMore() {
+        try {
+            const response = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=7a7de07845bd21df37d2fed08dc1f49c&offset=${currAllowed + 20}`);
+          if (!response.ok) {
+            throw new Error('Failed to fetch data from the API');
+          }
+          const data = await response.json();
+          
+          // Update the UI with the new results
+          displayMovies(data.results);
+          
+          // Increment the page number for the next fetch
+          currAllowed += 20;
+        } catch (error) {
+          console.error(error);
+        }
+      }
+
+    const loadButton = document.querySelector('#load-button')
+    loadButton.addEventListener('click', loadMore)
+      
+
