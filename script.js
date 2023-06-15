f47f93e82390cb68cb03af3b802984de63197f2c
Marley <marleyburrows1@gmail.com>
2023-06-14 22:04:47 -0700

    final project

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
-const API_KEY = '7a7de07845bd21df37d2fed08dc1f49c'
+const API_KEY = '7a7de07845bd21df37d2fed08dc1f49c';
+const currstate = {
+  page: 1,
+  searchTerm: ""
+};
 
+const searchForm = document.getElementById("search-form");
+const searchInput = document.getElementById("bar");
+const resultsEl = document.getElementById("results");
 
+const rootUrl = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&sort_by=popularity.desc&page=';
+const filterUrl = 'https://api.themoviedb.org/3/search/movie?include_adult=false&language=en-US&page=1';
+const apiKey = API_KEY;
 
-function generateCards(movieObject){
+function generateCards(movieObjects, append = false) {
+  if (!append) {
+    resultsEl.innerHTML = "";
+  }
+
+  movieObjects.forEach((movieObject) => {
     // creates star
     let star = document.createElement('span');
     star.classList.add('star');
@@ -38,44 +53,73 @@ function generateCards(movieObject){
     movieContainer.appendChild(avgContainer)
     movieContainer.appendChild(title)
     document.body.appendChild(movieContainer)
+
+    resultsEl.appendChild(movieContainer);
+  });
 }
 
+async function fetchMovieData(page) {
+  const url = rootUrl + `${page}&api_key=${apiKey}`;
 
+  try {
+    const response = await fetch(url);
+    const response_1 = await response.json();
+    return response_1.results;
+  } catch (err) {
+    console.error(err);
+    return [];
+  }
+}
 
-  
-  fetch('https://api.themoviedb.org/3/discover/movie?api_key=' + API_KEY)
-    .then(response => response.json())
-    .then(response => {
-        response.results.forEach(generateCards)
+function showInitialMovieCards() {
+  fetchMovieData(currstate.page)
+    .then(movieResults => {
+      const initialCards = movieResults.slice(0, 9); // Show only the first 9 movie cards
+      generateCards(initialCards);
+    });
+}
+
+async function searchMovies(searchTerm) {
+  try {
+    const response = await fetch(
+      `https://api.themoviedb.org/3/search/movie?include_adult=false&language=en-US&page=1&api_key=7a7de07845bd21df37d2fed08dc1f49c&query=${searchTerm}`
+      );
+    const data = await response.json();
+    console.log(data.results)
+    return data.results;
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+}
+
+function handleFormSubmit(event) {
+  event.preventDefault();
+  const searchTerm = searchInput.value.trim();
+  if (searchTerm !== "") {
+    searchMovies(searchTerm)
+      .then((searchResults) => {
+        console.log(searchResults)
+        generateCards(searchResults);
+      });
+  }
+}
+
+function loadMore() {
+  currstate.page += 1;
+
+  fetch(rootUrl + `${currstate.page}&api_key=${apiKey}`)
+    .then((response) => response.json())
+    .then((response) => {
+      const movieResults = response.results;
+      generateCards(movieResults, true);
     })
-    .catch(err => console.error(err));
+    .catch((err) => console.error(err));
+}
 
-
-    // function search() {
-
-    // }
-
-    let currAllowed = 0
-
-    async function loadMore() {
-        try {
-            const response = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=7a7de07845bd21df37d2fed08dc1f49c&offset=${currAllowed + 20}`);
-          if (!response.ok) {
-            throw new Error('Failed to fetch data from the API');
-          }
-          const data = await response.json();
-          
-          // Update the UI with the new results
-          displayMovies(data.results);
-          
-          // Increment the page number for the next fetch
-          currAllowed += 20;
-        } catch (error) {
-          console.error(error);
-        }
-      }
-
-    const loadButton = document.querySelector('#load-button')
-    loadButton.addEventListener('click', loadMore)
-      
-
+window.onload = function () {
+  searchForm.addEventListener("submit", handleFormSubmit);
+  const showMoreBtn = document.getElementById("show-more");
+  showMoreBtn.addEventListener("click", loadMore);
+  showInitialMovieCards();
+};
